17b14ac72ebe10d203f508a9e50594c22afc9c59
Lindsay Eggers <lindsayeggers@gmail.com>
2017-09-21 12:38:51 -0400

    vending maching

--- Capstone/Classes/VendingMachineItem.cs
+++ Capstone/Classes/VendingMachineItem.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Capstone.Classes
 {
-    class VendingMachingItem
+    public abstract class VendingMachineItem
     {
+        private string itemName;
+        private decimal priceInCents;
+        
+        public virtual string ItemName
+        {
+            get { return this.itemName; }
+        }
+
+        public decimal PriceInCents
+        {
+            get { return this.priceInCents; }
+        }
+
+        public abstract string Consume();
+
+        public VendingMachineItem(string itemName, decimal price)
+        {
+            this.itemName = itemName;
+            this.priceInCents = price;
+        }
+
     }
 }
