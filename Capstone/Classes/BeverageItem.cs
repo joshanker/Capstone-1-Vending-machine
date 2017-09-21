9d36538a4530181fc1bb4d6dee9a88ea5bb178ca
Lindsay Eggers <lindsayeggers@gmail.com>
2017-09-21 12:41:15 -0400

    beverage class

--- Capstone/Classes/BeverageItem.cs
+++ Capstone/Classes/BeverageItem.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Capstone.Classes;
 
 namespace Capstone.Classes
 {
-    class BeverageItem
+    public class BeverageItem : VendingMachineItem
     {
+        public BeverageItem(string itemName, decimal price) : base(itemName, price)
+        {
+        }
+
+        public override string Consume()
+        {
+            return "Glug Glug, Yuum!";
+        }
     }
 }
