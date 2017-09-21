5659178cd70d93d957697527102a9715569373dc
Lindsay Eggers <lindsayeggers@gmail.com>
2017-09-21 12:18:03 -0400

    built item classes

---
+++ Capstone/Classes/CandyItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capstone.Classes;
+
+namespace Capstone.Classes 
+{
+    public class CandyItem : VendingMachineItem
+    {
+        public CandyItem(string itemName, decimal price) : base(itemName, price)
+        {
+        }
+
+        public override string Consume()
+        {
+            return "munch munch, yum!";
+        }
+
+    }
+}
---
+++ Capstone/Classes/ChipItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capstone.Classes;
+
+namespace Capstone.Classes
+{
+    public class ChipItem : VendingMachineItem
+    {
+        public ChipItem(string itemName, decimal price) : base(itemName, price)
+        {
+        }
+
+        public override string Consume()
+        {
+            return "Crunch Crunch, Yum!";
+        }
+    }
+}
